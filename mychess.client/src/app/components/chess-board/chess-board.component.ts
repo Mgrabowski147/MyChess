7e96dd67aa578b4bf256e25a765b7b6749a8bdad
Maksymilian Grabowski <maksymilian.grabowski@axadirect-solutions.pl>
2024-04-07 20:15:20 +0200

    init

---
+++ mychess.client/src/app/components/chess-board/chess-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-board',
+  templateUrl: './chess-board.component.html',
+  styleUrl: './chess-board.component.css',
+  standalone: true
+})
+export class ChessBoardComponent {
+
+}
